9fbd15d494296ce440923d03611cf7cb4bbdbe79
matthijsreyers <matthijscrafter@gmail.com>
2024-10-05 13:34:57 +0200

    Added logical links for different icons

---
+++ status/generate-links.py
@@ -0,0 +1,15 @@
+import json
+import os
+
+sizes = [16,22,24]
+
+with open('links.json') as links:
+    links: dict = json.loads(links.read())
+    for (source, links) in links.items():
+        for size in sizes:
+            icon = f'./{size}/{source}'
+            if os.path.isfile(icon):
+                for link in links:
+                    icon_link = f'./{size}/{link}'
+                    os.system(f'ln -s {source} {icon_link}')
+                    # os.system(f'ln -s {source} {icon_link}')
